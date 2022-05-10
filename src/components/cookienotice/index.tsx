e698d33e02b0cc7683b1ce13f286ffa0985a009e
Víctor Díaz Marco <victor@diazmarco.me>
2022-05-10 13:17:14 +0200

    Add "SameSite" attribute to cookies.

--- src/components/cookienotice/index.tsx
+++ src/components/cookienotice/index.tsx
@@ -16,6 +16,7 @@ export default function CookieNotice(props: Props) {
       path: "/",
       maxAge: 365 * 24 * 60 * 60,
       secure: true,
+      sameSite: "strict",
     });
   }, [setCookie, accepted]);
 
